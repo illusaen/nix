1d2825dfddc2039abaaf8dca95979490fb70ee27
Wendy Chen <jaewchen@gmail.com>
2026-06-29 22:08:15 -0500

    "adding different styles per position for waybar"

--- wrappers/waybar/module.nix
+++ wrappers/waybar/module.nix
@@ -47,65 +47,13 @@
     };
     moduleSettings = {
       layer = "overlay";
-      width = 56;
       spacing = 16;
-
-      modules-left = [
-        "custom/notification"
-        "niri/workspaces"
-      ];
-      modules-center = ["group/date-time"];
-      modules-right = [
-        "custom/switcher"
-        "tray"
-      ];
       "niri/workspaces" = {
         format = "{icon}";
         format-icons = {
           active = "●";
           default = "○";
         };
-      };
-      "group/date-time" = {
-        orientation = "inherit";
-        modules = [
-          "clock#hour"
-          "clock#minute"
-          "clock#period"
-          "clock#weekday"
-          "clock#month"
-          "clock#day"
-        ];
-      };
-      "clock#hour" = clockSegment mono "15" "%I";
-      "clock#minute" = clockSegment mono "15" "%M";
-      "clock#period" = clockSegment mono "12" "%p";
-      "clock#weekday" = clockSegment sans "12" "%a";
-      "clock#month" = clockSegment sans "12" "%b";
-      "clock#day" = clockSegment mono "15" "%d";
-      tray = {
-        icon-size = 20;
-        spacing = 8;
-      };
-      "custom/notification" = let
-        swaync-client = lib.getExe' pkgs.local.swaync "swaync-client";
-      in {
-        exec = "${swaync-client} -swb";
-        exec-if = "which ${swaync-client}";
-        restart-interval = 5;
-        return-type = "json";
-        format = "{icon}";
-        format-icons = {
-          notification = "notifications";
-          none = "notifications_none";
-          dnd-notification = "notifications_off";
-          dnd-none = "notifications_off";
-          inhibited-notification = "notifications_off";
-          inhibited-none = "notifications_off";
-          dnd-inhibited-notification = "notifications_off";
-          dnd-inhibited-none = "notifications_off";
-        };
-        on-click = "${swaync-client} -t -sw";
       };
       "custom/switcher" = {
         format = "desktop_windows";
@@ -124,17 +72,69 @@
         margin-bottom = 16;
         margin-left = 8;
         margin-right = 8;
+        modules-left = [
+          "custom/notification"
+          "niri/workspaces"
+        ];
+        modules-center = ["group/date-time"];
+        modules-right = [
+          "custom/switcher"
+          "tray"
+        ];
+        "group/date-time" = {
+          orientation = "inherit";
+          modules = [
+            "clock#hour"
+            "clock#minute"
+            "clock#period"
+            "clock#weekday"
+            "clock#month"
+            "clock#day"
+          ];
+        };
+        "clock#hour" = clockSegment mono "15" "%I";
+        "clock#minute" = clockSegment mono "15" "%M";
+        "clock#period" = clockSegment mono "12" "%p";
+        "clock#weekday" = clockSegment sans "12" "%a";
+        "clock#month" = clockSegment sans "12" "%b";
+        "clock#day" = clockSegment mono "15" "%d";
+        tray = {
+          icon-size = 20;
+          spacing = 8;
+        };
+        "custom/notification" = let
+          swaync-client = lib.getExe' pkgs.local.swaync "swaync-client";
+        in {
+          exec = "${swaync-client} -swb";
+          exec-if = "which ${swaync-client}";
+          restart-interval = 5;
+          return-type = "json";
+          format = "{icon}";
+          format-icons = {
+            notification = "notifications";
+            none = "notifications_none";
+            dnd-notification = "notifications_off";
+            dnd-none = "notifications_off";
+            inhibited-notification = "notifications_off";
+            inhibited-none = "notifications_off";
+            dnd-inhibited-notification = "notifications_off";
+            dnd-inhibited-none = "notifications_off";
+          };
+          on-click = "${swaync-client} -t -sw";
+        };
       }
     )
     (
       moduleSettings
       // {
-        position = "top";
+        position = "bottom";
         output = config.monitors.secondary;
         margin-top = 8;
         margin-bottom = 8;
         margin-left = 16;
         margin-right = 16;
+        modules-right = ["clock" "custom/switcher"];
+        clock.format = "{:%a, %b %d %I:%M %p}";
       }
     )
   ];
