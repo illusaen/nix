169ad6ea39867d293e250a7bfd4d0eb964c80a77
Wendy Chen <jaewchen@gmail.com>
2026-07-10 11:15:49 -0500

    "adding more validation"

--- lib/fleet.nix
+++ lib/fleet.nix
@@ -15,11 +15,63 @@ _: let
     then []
     else [message];
 
+  duplicates = values: let
+    uniqueValues = unique values;
+  in
+    filter (
+      value:
+        builtins.length (filter (candidate: candidate == value) values) > 1
+    )
+    uniqueValues;
+
+  hostInterfaceAddresses = host:
+    concatLists (
+      map (
+        interfaceName: let
+          interface = host.networkInterfaces.${interfaceName};
+        in
+          (
+            if interface ? ipv4
+            then [
+              {
+                address = interface.ipv4;
+                family = "ipv4";
+              }
+            ]
+            else []
+          )
+          ++ (
+            if interface ? ipv6
+            then [
+              {
+                address = interface.ipv6;
+                family = "ipv6";
+              }
+            ]
+            else []
+          )
+      )
+      (attrNames (host.networkInterfaces or {}))
+    );
+
+  hostAddressEntries = fleet:
+    concatLists (
+      map (
+        name:
+          map (entry: entry // {host = name;})
+          (hostInterfaceAddresses fleet.hosts.${name})
+      )
+      (attrNames fleet.hosts)
+    );
+
   validateHost = fleet: name: host:
     require (hasAttr host.owner fleet.users) "host '${name}' owner '${host.owner}' does not exist"
     ++ require (host.platform == "nixos" || host.platform == "darwin") "host '${name}' has invalid platform '${host.platform}'"
     ++ require (host ? targetHost) "host '${name}' is missing targetHost"
-    ++ require (!(host.preservation.enable or false) || (host.preservation.disk or null) != null) "host '${name}' enables preservation without a disk";
+    ++ require (!(host.preservation.enable or false) || (host.preservation.disk or null) != null) "host '${name}' enables preservation without a disk"
+    ++ concatLists (map (feature:
+      require false "host '${name}' lists feature '${feature}' more than once")
+    (duplicates (host.features or [])));
 
   validateService = fleet: name: service:
     require (hasAttr service.primary fleet.hosts) "service '${name}' primary host '${service.primary}' does not exist"
@@ -35,10 +87,32 @@ _: let
       require (builtins.pathExists (../secrets/hosts + "/${name}/host_ed25519.pub")) "host '${name}' is missing secrets/hosts/${name}/host_ed25519.pub")
     (attrNames fleet.hosts));
 
+  validateUniqueHostValues = fleet: let
+    names = attrNames fleet.hosts;
+    duplicateTargetHosts = duplicates (map (name: fleet.hosts.${name}.targetHost) names);
+    hostsWithHostId = filter (name: fleet.hosts.${name} ? hostId) names;
+    duplicateHostIds = duplicates (map (name: fleet.hosts.${name}.hostId) hostsWithHostId);
+    addressEntries = hostAddressEntries fleet;
+    globallyUniqueAddressEntries =
+      filter (entry: entry.family != "ipv6" || builtins.substring 0 5 entry.address != "fe80:")
+      addressEntries;
+    duplicateAddresses = duplicates (map (entry: entry.address) globallyUniqueAddressEntries);
+  in
+    concatLists (map (targetHost:
+      require false "targetHost '${targetHost}' is used by multiple hosts")
+    duplicateTargetHosts)
+    ++ concatLists (map (hostId:
+      require false "hostId '${hostId}' is used by multiple hosts")
+    duplicateHostIds)
+    ++ concatLists (map (address:
+      require false "network address '${address}' is used by multiple hosts")
+    duplicateAddresses);
+
   validate = fleet:
     concatLists (map (name: validateHost fleet name fleet.hosts.${name}) (attrNames fleet.hosts))
     ++ concatLists (map (name: validateService fleet name fleet.services.${name}) (attrNames fleet.services))
-    ++ validateSecretHostKeys fleet;
+    ++ validateSecretHostKeys fleet
+    ++ validateUniqueHostValues fleet;
 
   assertValid = fleet: let
     errors = validate fleet;
@@ -56,5 +130,5 @@ _: let
       (filter (name: hosts.${name}.platform == platform) (attrNames hosts))
     );
 in {
-  inherit assertValid platformHosts unique validate;
+  inherit assertValid duplicates hostAddressEntries platformHosts unique validate;
 }
