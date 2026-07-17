5b7e29f11ab925bed5410dd1cc95be8f4f4e1e7b
Wendy Chen <jaewchen@gmail.com>
2026-07-16 20:24:27 -0500

    "removing 2nd manual import of hostLib"

--- hive.nix
+++ hive.nix
@@ -1,10 +1,6 @@
 let
   api = import ./default.nix;
   inherit (api) sources;
-  hostLib = import ./lib/hosts.nix {
-    inherit (api) featureLib fleetLib packageLib;
-    lib = api.nixpkgsLib;
-  };
   nixpkgs = import sources.nixpkgs.outPath {
     overlays = [api.packageLib.overlay];
   };
@@ -12,16 +8,12 @@ in
   {
     meta = {
       inherit nixpkgs;
-      specialArgs = {
-        inherit sources;
-        inherit (api) fleet;
-      };
     };
   }
   // builtins.mapAttrs (
     hostName: host: {
       imports = [
-        (hostLib.mkHostModule {
+        (api.hostLib.mkHostModule {
           inherit hostName host sources;
           inherit (api) fleet;
         })
