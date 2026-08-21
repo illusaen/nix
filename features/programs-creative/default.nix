cad7c666c05e972f5363d40dad0838150940157c
Wendy Chen <jaewchen@gmail.com>
2026-08-20 21:20:22 -0500

    adding bambu studio to persist

--- features/programs-creative/default.nix
+++ features/programs-creative/default.nix
@@ -1,34 +1,45 @@
 {
-  modules.nixos = {pkgs, ...}: {
-    environment.systemPackages = with pkgs; [
-      inkscape
-      bambu-studio
-      image-roll
-      onlyoffice-desktopeditors
-    ];
+  modules.nixos = {
+    pkgs,
+    lib,
+    options,
+    ...
+  }:
+    lib.mkMerge [
+      {
+        environment.systemPackages = with pkgs; [
+          inkscape
+          bambu-studio
+          image-roll
+          onlyoffice-desktopeditors
+        ];
 
-    xdg.mime.defaultApplications."image/*" = "com.github.weclaw1.ImageRoll.desktop";
+        xdg.mime.defaultApplications."image/*" = "com.github.weclaw1.ImageRoll.desktop";
 
-    nixpkgs.overlays = [
-      (_final: prev: {
-        bambu-studio = prev.bambu-studio.overrideAttrs (oldAttrs: {
-          buildInputs = oldAttrs.buildInputs or [];
-          postFixup =
-            (oldAttrs.postFixup or "")
-            + ''
-              wrapProgram $out/bin/bambu-studio \
-                --set GBM_BACKEND "dri" \
-                --set WEBKIT_DISABLE_DMABUF_RENDERER "1" \
-                --set WEBKIT_DISABLE_COMPOSITING_MODE "1" \
-                --set __GLX_VENDOR_LIBRARY_NAME "mesa" \
-                --set __EGL_VENDOR_LIBRARY_FILENAMES "/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json" \
-                --set MESA_LOADER_DRIVER_OVERRIDE "zink" \
-                --set GALLIUM_DRIVER "zink"
-            '';
-        });
+        nixpkgs.overlays = [
+          (_final: prev: {
+            bambu-studio = prev.bambu-studio.overrideAttrs (oldAttrs: {
+              buildInputs = oldAttrs.buildInputs or [];
+              postFixup =
+                (oldAttrs.postFixup or "")
+                + ''
+                  wrapProgram $out/bin/bambu-studio \
+                    --set GBM_BACKEND "dri" \
+                    --set WEBKIT_DISABLE_DMABUF_RENDERER "1" \
+                    --set WEBKIT_DISABLE_COMPOSITING_MODE "1" \
+                    --set __GLX_VENDOR_LIBRARY_NAME "mesa" \
+                    --set __EGL_VENDOR_LIBRARY_FILENAMES "/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json" \
+                    --set MESA_LOADER_DRIVER_OVERRIDE "zink" \
+                    --set GALLIUM_DRIVER "zink"
+                '';
+            });
+          })
+        ];
+      }
+      (lib.mkIf (options ? persistUser) {
+        persistUser.directories = [".config/BambuStudio"];
       })
     ];
-  };
 
   modules.darwin = {
     homebrew = {
